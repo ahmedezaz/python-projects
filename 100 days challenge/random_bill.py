a3defd66ea36026efba328057ffb822c52c5d9fd
ahmedezaz <ezaz716@gmail.com>
2025-08-15 18:51:21 +0100

    Randomly selected from the list

---
+++ 100 days challenge/random_bill.py
@@ -0,0 +1,6 @@
+import random
+friends = ["Sheldon", "Amy", "Leonard", "Penny", "Howard", "Bernadette", "Raj",]
+
+select = random.randint(0,6)
+
+print(f"Today {friends[select]} will pay the bill.")
